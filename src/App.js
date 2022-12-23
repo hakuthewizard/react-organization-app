b6722b8776a310ad24319da36db6adf06488a8e4
Beyza Ballı <hakuthegeek@gmail.com>
2022-12-24 00:32:56 +0300

    update on routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import {Route, Routes,Redirect} from 'react-router-dom';
-import { Fragment } from 'react';
+import {Route, Routes} from 'react-router-dom';
 import NotesPage from './Pages/NotesPage';
 import ListsPage from './Pages/ListsPage';
 import Navbar from './components/NavigationBar/Navbar';
 import EditableCalendar from './Pages/CalendarPage';
+import Welcome from './Pages/Welcome'
 import './App.css';
 
 function App() {
     return (
         <div>
             <Navbar/>
+            <Welcome/>
             <main className='main'>
         <Routes>
-            <Route path="/notes" element={<NotesPage />}/>
+            <Route path="/" element={<NotesPage />} exact/>
+                
+            <Route path="/notes" element={<NotesPage />} exact/>
                 
             <Route path="/lists" element={<ListsPage />}/>
             <Route path="/calendar" element={<EditableCalendar />}/>
---
+++ src/Pages/Welcome.js
@@ -0,0 +1,9 @@
+const Welcome = () => {
+return(
+    <div>
+    <header style={{textAlign:"center"}}>Welcome to your online digital organizer, here you can add notes, add a to-do list and set reminders on the calendar! Have fun!
+    </header>
+    </div>
+)
+}
+export default Welcome;
